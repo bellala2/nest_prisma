885284ed64b9641a95a1f216153f391d0e52c497
Bella Nur Hidayah <bellanurhidayah08@gmail.com>
2026-04-20 12:18:51 +0700

    prepare deployment

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,11 @@ async function bootstrap() {
     ) 
     .build();
     const document = SwaggerModule.createDocument(app, config); 
-  SwaggerModule.setup('api', app, document); 
+  SwaggerModule.setup('api', app, document, {
+  swaggerOptions: {
+    persistAuthorization: true,
+  },
+});
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
